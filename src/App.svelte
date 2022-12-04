c6cf40b6255aa292700fd03daaaf5f4406ffbfa5
Prerak Mann <mannprerak2@gmail.com>
2022-12-05 00:50:39 +0530

    Run build without csvv

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,30 @@
 <script>
-import {Network} from "./newton/network/network.js"
-import {Graph} from "./newton/graph/graph.js"
+  import { onMount } from "svelte";
+  import { Network } from "./newton/network/network.js";
+  import { Graph } from "./newton/graph/graph.js";
 
-import * as data from "./data/data.js"
+  import * as data from "./data/data.js";
 
-const network = new Network(data.nodes, data.links)
-const graph = new Graph({
-  width: window.innerWidth,
-  height: window.innerHeight,
-  flow: 'horizontal',
-  // engine: 'd3',
-  draggable: true,
-  network: network
-})
-graph.init()
+  onMount(async () => {
+    const network = new Network(data.nodes, data.links);
+    const graph = new Graph({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      flow: "horizontal",
+      // engine: 'd3',
+      draggable: true,
+      network: network,
+    });
+    graph.init();
 
-graph.on('node:click', (n) => graph.highlightDependencies(n, { arrows: true }))
+    graph.on("node:click", (n) =>
+      graph.highlightDependencies(n, { arrows: true })
+    );
+  });
 </script>
 
 <main>
-  <h1>Hello</h1>
-  <svg class="graph-svg" width="960" height="600"></svg>
+  <svg class="graph-svg" width="960" height="600" />
 </main>
 
 <style>
