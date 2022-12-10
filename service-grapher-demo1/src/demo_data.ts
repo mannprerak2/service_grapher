394a9729166b8b4af847423ff00ee8cec53133b3
Prerak Mann <mannprerak2@gmail.com>
2022-12-10 23:03:43 +0530

    update demo_data

--- service-grapher-demo1/src/demo_data.ts
+++ service-grapher-demo1/src/demo_data.ts
@@ -4,23 +4,53 @@ const data = {
     nodes: [
         {
             label: "Frontend",
-            tags: ["P0"]
+            tags: ["P0"],
+            extra: [
+                {
+                    "key": "URL",
+                    "value": "https://some-frontend.com"
+                },
+            ],
         },
         {
             label: "API Gateway",
-            tags: ["P0"]
+            tags: ["P0"],
+            extra: [
+                {
+                    "key": "Authentication Type",
+                    "value": "JWT"
+                },
+            ],
         },
         {
             label: "Backend",
-            tags: ["P0"]
+            tags: ["P0"],
+            extra: [
+                {
+                    "key": "Tech Stack",
+                    "value": "Django (3.2) | Python (3.6)"
+                },
+            ],
         },
         {
             label: "Logging Service",
-            tags: ["P1"]
+            tags: ["P1"],
+            extra: [
+                {
+                    "key": "Sentry URL",
+                    "value": "My-Sentry.sentry.io"
+                },
+            ],
         },
         {
             label: "Database",
-            tags: ["P0"]
+            tags: ["P0"],
+            extra: [
+                {
+                    "key": "MySQL",
+                    "value": "Version: 5.5"
+                },
+            ],
         },
     ],
     links: [
