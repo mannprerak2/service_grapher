e9a1a5afb8357c86f3202ecf125fcdbd5da7cad3
Prerak Mann <mannprerak2@gmail.com>
2022-12-10 14:41:38 +0530

    Fix allignment

--- src/App.svelte
+++ src/App.svelte
@@ -6,10 +6,12 @@
   import * as data from "./data/data.js";
 
   onMount(async () => {
+    let graphUI = window.document.getElementById("main-free-area")
     const network = new Network(data.nodes, data.links);
     const graph = new Graph({
-      width: window.innerWidth,
-      height: window.innerHeight,
+      width: graphUI.clientWidth,
+      height: graphUI.clientHeight,
+      margin: 0,
       flow: "horizontal",
       // engine: 'd3',
       draggable: true,
@@ -24,9 +26,53 @@
   });
 </script>
 
-<main>
-  <svg class="graph-svg" width="960" height="600" />
-</main>
+<!-- <main>
+  <svg class="graph-svg" width="50vw" />
+</main> -->
+<div class="container-table">
+  <div id="left-free-area">
+    <div id="top-bar">
+        TOP AREA
+    </div>
+    <svg id="main-free-area" class="graph-svg" />
+  </div>
+
+  <div id="right-fixed-bar">
+  </div>
+</div>
 
 <style>
+  .container-table {
+    display: table;
+    margin: 0 0;
+  }
+
+  #right-fixed-bar {
+    width: 20vw;
+    height: 100vh;
+    display: table-cell;
+    overflow: scroll;
+    height: 100%;
+    border-left: 1px solid gray;
+    margin: 0 0;
+    padding: 10px;
+  }
+
+  #left-free-area {
+    display: table-cell;
+    width: 80vw;
+    height: 100vh;
+    margin: 0 0;
+  }
+
+  #top-bar {
+    width: 100%;
+    height: 10vh;
+    border-bottom: 1px solid gray;
+  }
+
+  #main-free-area {
+    width: 100%;
+    height: 90vh;
+  }
 </style>
